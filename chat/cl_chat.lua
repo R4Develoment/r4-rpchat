f32f9246dea05685e26d9a77a4664e1031d09583
rodriellin4 <68621059+rodriellin4@users.noreply.github.com>
2023-02-04 18:45:21 +0100

    Add files via upload

---
+++ chat/cl_chat.lua
@@ -0,0 +1,263 @@
+local chatInputActive = false
+local chatInputActivating = false
+local chatHidden = true
+local chatLoaded = false
+s = 100
+_s = 2000
+RegisterNetEvent('chatMessage')
+RegisterNetEvent('chat:addTemplate')
+RegisterNetEvent('chat:addMessage')
+RegisterNetEvent('chat:addSuggestion')
+RegisterNetEvent('chat:addSuggestions')
+RegisterNetEvent('chat:removeSuggestion')
+RegisterNetEvent('chat:clear')
+
+-- internal events
+RegisterNetEvent('__cfx_internal:serverPrint')
+
+RegisterNetEvent('_chat:messageEntered')
+
+--deprecated, use chat:addMessage
+AddEventHandler('chatMessage', function(author, color, text)
+  local args = { text }
+  if author ~= "" then
+    table.insert(args, 1, author)
+  end
+  SendNUIMessage({
+    type = 'ON_MESSAGE',
+    message = {
+      color = color,
+      multiline = true,
+      args = args
+    }
+  })
+end)
+
+AddEventHandler('__cfx_internal:serverPrint', function(msg)
+  print(msg)
+
+  SendNUIMessage({
+    type = 'ON_MESSAGE',
+    message = {
+      templateId = 'print',
+      multiline = true,
+      args = { msg }
+    }
+  })
+end)
+
+AddEventHandler('chat:addMessage', function(message)
+  SendNUIMessage({
+    type = 'ON_MESSAGE',
+    message = message
+  })
+end)
+
+AddEventHandler('chat:addSuggestion', function(name, help, params)
+  SendNUIMessage({
+    type = 'ON_SUGGESTION_ADD',
+    suggestion = {
+      name = name,
+      help = help,
+      params = params or nil
+    }
+  })
+end)
+
+AddEventHandler('chat:addSuggestions', function(suggestions)
+  for _, suggestion in ipairs(suggestions) do
+    SendNUIMessage({
+      type = 'ON_SUGGESTION_ADD',
+      suggestion = suggestion
+    })
+  end
+end)
+
+AddEventHandler('chat:removeSuggestion', function(name)
+  SendNUIMessage({
+    type = 'ON_SUGGESTION_REMOVE',
+    name = name
+  })
+end)
+
+AddEventHandler('chat:addTemplate', function(id, html)
+  SendNUIMessage({
+    type = 'ON_TEMPLATE_ADD',
+    template = {
+      id = id,
+      html = html
+    }
+  })
+end)
+
+AddEventHandler('chat:clear', function(name)
+  SendNUIMessage({
+    type = 'ON_CLEAR'
+  })
+end)
+
+
+
+RegisterNUICallback('chatResult', function(data, cb)
+  chatInputActive = false
+  SetNuiFocus(false)
+
+  if not data.canceled then
+    local id = PlayerId()
+
+    --deprecated
+    local r, g, b = 0, 0x99, 255
+
+    if data.message:sub(1, 1) == '/' then
+      ExecuteCommand(data.message:sub(2))
+    else
+      TriggerServerEvent('_chat:messageEntered', GetPlayerName(id), { r, g, b }, data.message)
+    end
+  end
+
+  cb('ok')
+end)
+
+local function refreshCommands()
+  if GetRegisteredCommands then
+    local registeredCommands = GetRegisteredCommands()
+
+    local suggestions = {}
+
+    for _, command in ipairs(registeredCommands) do
+        if IsAceAllowed(('command.%s'):format(command.name)) then
+            table.insert(suggestions, {
+                name = '/' .. command.name,
+                help = ''
+            })
+        end
+    end
+
+    TriggerEvent('chat:addSuggestions', suggestions)
+  end
+end
+
+local function refreshThemes()
+  local themes = {}
+
+  for resIdx = 0, GetNumResources() - 1 do
+    local resource = GetResourceByFindIndex(resIdx)
+
+    if GetResourceState(resource) == 'started' then
+      local numThemes = GetNumResourceMetadata(resource, 'chat_theme')
+
+      if numThemes > 0 then
+        local themeName = GetResourceMetadata(resource, 'chat_theme')
+        local themeData = json.decode(GetResourceMetadata(resource, 'chat_theme_extra') or 'null')
+
+        if themeName and themeData then
+          themeData.baseUrl = 'nui://' .. resource .. '/'
+          themes[themeName] = themeData
+        end
+      end
+    end
+  end
+
+  SendNUIMessage({
+    type = 'ON_UPDATE_THEMES',
+    themes = themes
+  })
+end
+
+AddEventHandler('onClientResourceStart', function(resName)
+  Wait(_s)
+
+  refreshCommands()
+  refreshThemes()
+end)
+
+AddEventHandler('onClientResourceStop', function(resName)
+  Wait(_s)
+
+  refreshCommands()
+  refreshThemes()
+end)
+
+RegisterNUICallback('loaded', function(data, cb)
+  TriggerServerEvent('chat:init');
+
+  refreshCommands()
+  refreshThemes()
+
+  chatLoaded = true
+
+  cb('ok')
+end)
+
+Citizen.CreateThread(function()
+    while true do
+        for id = 0, 255 do
+            if GetPlayerPed(-1) then
+                x1, y1, z1 = table.unpack(GetEntityCoords(GetPlayerPed(-1), true))
+                distance = math.floor(GetDistanceBetweenCoords(x1,  y1,  z1,  x2,  y2,  z2,  true))
+            end
+        end
+        Citizen.Wait(1000)
+    end
+end)
+
+Citizen.CreateThread(function()
+  SetTextChatEnabled(false)
+  SetNuiFocus(false)
+
+  while true do
+    Wait(s)
+
+    if not chatInputActive then
+      if IsControlPressed(0, 245) --[[ INPUT_MP_TEXT_CHAT_ALL ]] then
+        chatInputActive = true
+        chatInputActivating = true
+
+        SendNUIMessage({
+          type = 'ON_OPEN'
+        })
+      end
+    end
+
+    if chatInputActivating then
+      if not IsControlPressed(0, 245) then
+        SetNuiFocus(true)
+
+        chatInputActivating = false
+      end
+    end
+
+    if chatLoaded then
+      local shouldBeHidden = false
+
+      if IsScreenFadedOut() or IsPauseMenuActive() then
+        shouldBeHidden = true
+      end
+
+      if (shouldBeHidden and not chatHidden) or (not shouldBeHidden and chatHidden) then
+        chatHidden = shouldBeHidden
+
+        SendNUIMessage({
+          type = 'ON_SCREEN_STATE_CHANGE',
+          shouldHide = shouldBeHidden
+        })
+      end
+    end
+  end
+end)
+
+function AderNotify(x, y, z, text)
+    local onScreen,_x,_y=World3dToScreen2d(x,y,z)
+    local factor = #text / 370
+    local px,py,pz=table.unpack(GetGameplayCamCoords())
+    
+    SetTextScale(0.35, 0.35)
+    SetTextFont(0)
+    SetTextProportional(1)
+    SetTextColour(255, 255, 255, 215)
+    SetTextEntry("STRING")
+    SetTextCentre(1)
+    AddTextComponentString(text)
+    DrawText(_x,_y)
+    DrawRect(_x,_y + 0.0125, 0.060 + factor, 0.035, 0, 0, 0, 190)
+end
---
+++ chat/fxmanifest.lua
@@ -0,0 +1,29 @@
+fx_version'adamant'
+
+game 'gta5'
+
+description 'Chat Design Modificated by Nightt#8899 and redesigned by rodriellin4#4869'
+
+author 'rodriellin4#4869'
+
+ui_page 'html/index.html'
+
+client_script 'cl_chat.lua'
+
+server_script 'sv_chat.lua'
+
+files {
+    'html/index.html',
+    'html/index.css',
+    'html/config.default.js',
+    'html/config.js',
+    'html/App.js',
+    'html/Message.js',
+    'html/Suggestions.js',
+    'html/vendor/vue.2.3.3.min.js',
+    'html/vendor/flexboxgrid.6.3.1.min.css',
+    'html/vendor/animate.3.5.2.min.css',
+    'html/vendor/latofonts.css',
+    'html/franklin.ttf',
+    'html/BoxedRound.ttf',
+  }
